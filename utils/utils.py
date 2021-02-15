f3b2572d53ddca2feecd7975c84153d0816511bf
syoamakase <syoamakase@gmail.com>
2021-02-15 16:49:35 +0900

    add dafault parameters

--- utils/utils.py
+++ utils/utils.py
@@ -157,7 +157,7 @@ def overwrite_hparams(args):
 
 def fill_variables(hp):
     default_var = {'pe_alpha': False, 'stop_lr_change': 100000000, 'feed_forward': 'linear', 'optimizer': 'adam', 'mel_dim':80, 'is_flat_start':False,'dataset_shuffle_all':False, 'optimizer_type': 'Noam', 'init_lr':1e-3, 'save_per_epoch': 50, 'save_attention_per_step': 2000,
-                    'accum_grad':1, 'N_e':12, 'N_d':6, 'heads':4, 'd_model_e':256, 'd_model_d':256, 'encoder': None, 'amp': False, 'comment':''}
+                    'accum_grad':1, 'N_e':12, 'N_d':6, 'heads':4, 'd_model_e':256, 'd_model_d':256, 'encoder': None, 'amp': False, 'comment':'', 'granularity':1, 'subsampling_rate': 4}
     for key, value in default_var.items():
         if not hasattr(hp, key):
             print('{} is not found in hparams. defalut {} is used.'.format(key, value))
